bd182ca249c041b2133a329e1d443e6d44ffe94f
vincenzo.petronio <vincenzo.petronio@gmail.com>
2020-07-31 18:03:59 +0200

    ADD server with is4aspid template

---
+++ IdentityAuthorizationServer/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using IdentityAuthorizationServer.Data;
+using IdentityAuthorizationServer.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdentityAuthorizationServer.Areas.Identity.IdentityHostingStartup))]
+namespace IdentityAuthorizationServer.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
